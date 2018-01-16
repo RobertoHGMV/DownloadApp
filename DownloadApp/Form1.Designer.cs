9714613b329a36930497f6a4fc31e4fbe37af28f
robertohgmv <robertohgm@hotmail.com>
2018-01-15 22:35:11 -0200

    first commit

---
+++ DownloadApp/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace DownloadApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lbPath = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(108, 62);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(521, 26);
+            this.txtUrl.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL:";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDownload.Location = new System.Drawing.Point(637, 58);
+            this.btnDownload.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(112, 35);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(299, 156);
+            this.lbStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(112, 20);
+            this.lbStatus.TabIndex = 3;
+            this.lbStatus.Text = "Progresso: 0%";
+            this.lbStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(108, 102);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(523, 35);
+            this.progressBar.TabIndex = 4;
+            // 
+            // lbPath
+            // 
+            this.lbPath.AutoSize = true;
+            this.lbPath.Location = new System.Drawing.Point(24, 28);
+            this.lbPath.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbPath.Name = "lbPath";
+            this.lbPath.Size = new System.Drawing.Size(76, 20);
+            this.lbPath.TabIndex = 5;
+            this.lbPath.Text = "Caminho:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(108, 22);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(521, 26);
+            this.txtPath.TabIndex = 6;
+            // 
+            // btnPath
+            // 
+            this.btnPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPath.Location = new System.Drawing.Point(637, 18);
+            this.btnPath.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(112, 35);
+            this.btnPath.TabIndex = 1;
+            this.btnPath.Text = "Localizar";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 202);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lbPath);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrl);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(791, 241);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(791, 241);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lbPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnPath;
+    }
+}
+
---
+++ DownloadApp/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DownloadApp
+{
+    public partial class Form1 : Form
+    {
+        private WebClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            client = new WebClient();
+            client.DownloadProgressChanged += Client_DownloadProgressChanged;
+            client.DownloadFileCompleted += Client_DownloadFileCompleted;
+        }
+
+        private void Client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            lbStatus.Text = "Download finalizado!";
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            try
+            {
+                Invoke(new MethodInvoker(delegate ()
+                {
+                    progressBar.Minimum = 0;
+                    var receive = double.Parse(e.BytesReceived.ToString());
+                    var total = double.Parse(e.TotalBytesToReceive.ToString());
+                    var percentage = receive / total * 100;
+                    lbStatus.Text = $@"Progresso: {$"{percentage:0.##}"}%";
+                    progressBar.Value = int.Parse(Math.Truncate(percentage).ToString());
+                }));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Mensagem do Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ValidFields();
+
+                var url = txtUrl.Text;
+                var path = txtPath.Text;
+
+                var thread = new Thread(() =>
+                {
+                    var uri = new Uri(url);
+                    var fileName = System.IO.Path.GetFileName(uri.AbsolutePath);
+                    //client.DownloadFileAsync(uri, Application.StartupPath + "/" + fileName);
+                    client.DownloadFileAsync(uri, path  + "/" + fileName);
+                });
+                thread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Mensagem do Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var folderDialog = new FolderBrowserDialog())
+                {
+                    if (folderDialog.ShowDialog() == DialogResult.OK)
+                        txtPath.Text = folderDialog.SelectedPath;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Mensagem do Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ValidFields()
+        {
+            if (string.IsNullOrEmpty(txtUrl.Text))
+                throw new Exception("Informe a url!");
+
+            if (string.IsNullOrEmpty(txtPath.Text))
+                throw new Exception("Informe o caminho para salvar o arquivo!");
+        }
+    }
+}
